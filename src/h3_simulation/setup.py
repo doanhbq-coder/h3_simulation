805c9b212be1543edad6cf52be08020df948130c
doanhbq-coder <doanhbq@pheniakaa-x.com>
2026-03-25 11:01:21 +0700

    add src

---
+++ src/h3_simulation/setup.py
@@ -0,0 +1,37 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+from ament_index_python.packages import get_package_share_directory
+
+package_name = 'h3_simulation'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.xacro') + glob('urdf/*.urdf')),
+        (os.path.join('share', package_name, 'rviz'), glob('rviz/*.rviz')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+        (os.path.join('share', package_name, 'world'), glob('world/*.world')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='neo',
+    maintainer_email='doanhbq@pheniakaa-x.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
